6a85f7cee4b09ee3dca92455df0def21d111d8b5
Nuno Leite <nunocsleite@gmail.com>
2018-03-05 15:14:20 +0000

    Aulas 07-08

---
+++ Semana 3/Aulas 07-08/Reflection/Reflection/Reflection/PropertyInfo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CodeReflection
+{
+    class Student
+    {
+        public string Name { get; private set; }
+        public int Number { get; private set; }
+        public Student(int nr, string name)
+        {
+            Number = nr;
+            Name = name;
+        }
+    }
+
+
+    class Program
+    {
+        public static void ShowProperties(object o)
+        {
+			// Non-optimised version
+			Console.WriteLine ("Non-optimised version");
+            Type oType = o.GetType();
+            MemberInfo[] ms = oType.GetMembers();
+            foreach (MemberInfo m in ms)
+            {
+                PropertyInfo p = m as PropertyInfo;
+                if (p != null)
+                {
+                    MethodInfo pGet = p.GetGetMethod();
+                    object val = pGet.Invoke(o, null);
+                    Console.WriteLine("{0} = {1}", p.Name, val);
+                }
+
+            }
+
+            // Optimised version
+			Console.WriteLine ("Optimised version");
+			Type oType1 = o.GetType();
+            PropertyInfo[] ps = oType1.GetProperties();
+            foreach (PropertyInfo p in ps) {
+                object val = p.GetValue(o, null);
+                Console.WriteLine("{0} = {1}", p.Name, val);
+            }
+
+        }
+
+        static void Main1(string[] args)
+        {
+            ShowProperties(new Student(12, "rui"));
+        }
+      
+    }
+}

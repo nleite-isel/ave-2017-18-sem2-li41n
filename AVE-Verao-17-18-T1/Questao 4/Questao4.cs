6805b94d85dacb83b21a640a36dc25d10ca12f0a
Nuno Leite <nunocsleite@gmail.com>
2018-07-02 19:07:40 +0100

    Resolucao AVE-Verao-17-18-T1 - Questoes 1, 2, 3

---
+++ AVE-Verao-17-18-T1/Questao 4/Questao4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AVE_T1_SEM_INV_1718.Questao4
+{
+    delegate void Func(object o, int i);
+
+    class Del
+    {
+        private ArrayList handlers;
+
+        public Del(Func func) 
+        {
+            handlers = new ArrayList(); 
+            handlers.Add(func); 
+        }
+        public Func GetHandler(int idx) { 
+            return (Func) handlers[idx]; 
+        }
+    }
+    class MainClass {
+        private static void M(object o, int i) {
+            //...
+        }
+
+        public static void Main1() 
+        {
+            Del dels = new Del(M);
+            dels.GetHandler(0)(null, 10);
+        }
+    }
+}
+
+// Ver IL no ficheiro dump....il na diretoria
---
+++ AVE-Verao-17-18-T1/Questao 5/QuestaoGroupBy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AVEVerao1718T1.Questao5
+{
+    public static class ExtensionMethods {
+        public static IEnumerable<IEnumerable<T>> GroupBy<T, W>(this IEnumerable<T> seq, Func<T, W> func) 
+        {
+            Console.WriteLine("GroupBy");
+            
+            List<W> groups = new List<W>();
+            foreach (var item in seq)
+            {
+                W w = func(item);
+                if (!groups.Contains(w))
+                {
+                    yield return GetGroup(seq, func, w);
+                    // Add w to list of criteria already processed
+                    groups.Add(w);
+                }
+            }
+        }
+
+        private static IEnumerable<T> GetGroup<T, W>(IEnumerable<T> seq, Func<T, W> func, W w)
+        {
+            foreach (var item in seq)
+            {
+                if (func(item).Equals(w))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+
+    public class QuestaoGroupBy
+    {
+        public static void Main()
+        {
+            IEnumerable<string> words = new string[]
+                   { "isel", "k", "ola", "vue", "z", "soup", "kat", "bart" };
+
+            IEnumerable<IEnumerable<string>> res = words.GroupBy(w => w.Length);
+
+            foreach (IEnumerable<string> w in res)
+                Console.WriteLine(String.Join(",", w));
+        }
+
+    }
+
+}
+
